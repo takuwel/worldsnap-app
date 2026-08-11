b665855ffe0158fcdac98151d0ac30d994d2efc7
takuwel <takumishimodate88@gmail.com>
2026-08-11 21:17:45 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import {
   Search,
